15bb56524b82c6cae201fd586ca0a6404769d5e2
thamirespereira <thamiresfrpereira@hotmail.com>
2023-06-16 14:27:48 -0300

    exemplo calculadora imc

---
+++ exemplo2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+main(){
+float altura, peso, imc;
+printf("Entre com a sua altura e o seu peso.\n");
+scanf("%f %f", &altura, &peso);
+imc = (peso/altura)/altura;
+printf("Seu IMC vale %f\n", imc);
+}
